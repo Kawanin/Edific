939ac87f760015a2a216b1b4c4d1417554567f26
Vinnycios <vinnyciosdev@gmail.com>
2024-06-21 15:25:24 -0300

    card 2 complete

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ import Card from './components/Card.vue'
 import LocalCard from './components/LocalCard.vue'
 import image2 from "./assets/mapa2.webp"
 import ContactBar from './components/ContactBar.vue'
+import Card2 from './components/Card2.vue'
+import image0 from './assets/tab1.webp'
 </script>
 
 <template>
@@ -25,11 +27,12 @@ import ContactBar from './components/ContactBar.vue'
   <RouterView /> -->
 
 
-
+  <!-- <Card2 :image="image0" description="Possuímos corpo de engenharia especializado em engenharia diagnóstica para garantir as melhores soluções técnicas."/> -->
   <!-- <ContactBar email="contato@edificcontrutora.com" telephone="(44) 999915783" instagram="/tabengenharia"/>
   <LocalCard title="LOCAL 2" subtitle="R. São Cristóvão, 57 – ZONA 8 – Zona 08, Maringá – PR, 87050-490" telephone="(43) 99987-5018" :image="image2"/>
   <QueroContratarButton/>
   <Card title="INSPEÇÃO PREDIAL:" subtitle="O Laudo Cautelar de Vizinhança ou simplesmente Laudo de Vizinhança, é o documento que visa evitar conflitos desnecessárias em casos de danos causados por uma obra em imóveis localizados em seu entorno."/> -->
+  
 
 </template>
 
---
+++ src/components/Card2.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <img :src="image" :alt="imageDescription">
+        <p>{{ description }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    image: {
+        image: String,
+        required: true
+    },
+    description: {
+        description: String,
+        required: true
+    },
+    subtimageDescriptionitle: {
+        description: String,
+        required: false
+    }
+})
+</script>
+
+
+<style scoped>
+div{
+    display: flex;
+    flex-direction: row;
+    height: 140px;
+    max-width: 480px;
+    padding: 10px;
+}
+img{
+    margin-right: 20px;
+    filter: brightness(0%);
+}
+@media (max-width: 420px) {
+    div{
+        flex-direction: column;
+        height: fit-content;
+        align-items: center;
+        text-align: center;
+    }
+    img{
+        margin-right: 0px;
+        width: 45vw;
+        margin-bottom: 20px;
+    }
+}
+</style>
