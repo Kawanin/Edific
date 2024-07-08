f6c2cb36d6f5dea6fff3d8e133f1b0d56f4f0d32
Kawanin <l.kawano,jr@gmail.com>
2024-07-08 16:04:28 -0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,9 +6,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/edific/',
-
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
